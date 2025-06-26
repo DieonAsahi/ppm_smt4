b7011a587e4b8c216fb19879cde4944bd4a809bb
DieonAsahi <athawaguth@gmail.com>
2025-06-27 01:38:56 +0700

    Brono

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// JavaScript for Sidebar
 const sidebar = document.getElementById('sidebar');
 const overlay = document.querySelector('.overlay');
 const sidebarToggle = document.querySelector('.sidebar-toggle');
@@ -20,7 +19,6 @@ function closeSidebar() {
     document.body.style.overflow = 'auto';
 }
 
-// JavaScript for Slider
 let currentIndex = 0;
 const productList = document.querySelector('.product-list');
 const productItems = document.querySelectorAll('.product-item');
@@ -38,9 +36,13 @@ document.querySelector('.prev-btn').addEventListener('click', () => {
 document.querySelector('.next-btn').addEventListener('click', nextSlide);
 
 function updateSlider() {
-    const offset = -currentIndex * 25; // Adjust this based on item width
-    productList.style.transform = `translateX(${offset}%)`;
+    const item = productItems[0];
+    const itemWidth = item.getBoundingClientRect().width;
+    const offset = -currentIndex * itemWidth;
+
+    productList.style.transform = `translateX(${offset}px)`;
 }
+
 
 function nextSlide() {
     if (currentIndex < totalProducts - 1) {
@@ -51,4 +53,4 @@ function nextSlide() {
     updateSlider();
 }
 
-setInterval(nextSlide, 3000); // Autoplay every 3 seconds
+setInterval(nextSlide, 3000); 
